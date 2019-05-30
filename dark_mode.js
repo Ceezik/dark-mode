f32aeaed0e78f0162b3f82cad35bae69b88395c8
Clément KOENIG <clement.koenig@unistra.fr>
2019-05-30 10:51:49 +0200

    Add mode-switcher

---
+++ dark_mode.js
@@ -0,0 +1,15 @@
+let titleElt = document.getElementById('title')
+let iconElt = document.getElementsByTagName('ion-icon')[0]
+
+document.querySelector('.switch input[type="checkbox"]').addEventListener('change', function(event){
+    if (event.target.checked) {
+        document.body.setAttribute('data-theme', 'dark')
+        titleElt.innerText = 'dark'
+        iconElt.setAttribute('name', 'moon')
+
+    } else {
+        document.body.removeAttribute('data-theme')
+        titleElt.innerText = 'light'
+        iconElt.setAttribute('name', 'sunny')
+    }
+});
